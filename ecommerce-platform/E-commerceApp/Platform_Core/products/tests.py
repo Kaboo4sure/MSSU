a062650bb3a61082f386d6038b7a0d1a346fe39f
kaboo4sure <kaboo4real@gmail.com>
2025-04-13 22:28:22 -0500

    testing

--- ecommerce-platform/E-commerceApp/Platform_Core/products/tests.py
+++ ecommerce-platform/E-commerceApp/Platform_Core/products/tests.py
@@ -1,23 +1,19 @@
 from django.test import TestCase
-from products.models import Product
 from django.contrib.auth import get_user_model
-User = get_user_model()
 
 class ProductTests(TestCase):
     def setUp(self):
+        User = get_user_model()
         self.user = User.objects.create_user(username='seller1', password='pass123')
+        self.client.login(username='seller1', password='pass123')
 
     def test_add_product(self):
-        self.client.login(username='seller1', password='pass123')
-        response = self.client.post('/products/add/', {
+        response = self.client.post('/products/seller/add/', {
             'name': 'Test Product',
             'price': 50.00,
             'stock': 10,
-            'description': 'A product for test'
+            'description': 'Test description'
         })
         self.assertEqual(response.status_code, 302)  # redirect on success
 
-    def test_product_str(self):
-        product = Product(name='Item', price=10.00)
-        self.assertEqual(str(product), 'Item')
 
